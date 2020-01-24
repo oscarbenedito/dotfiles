ced5b5ed63fd6a4e707a2e3e5eb9f3b1e8959817
oscarbenedito <oscar@obenedito.org>
2020-01-24 02:01:55 +0100

    Fixed bugs and added aliases

--- dot_scripts/executable_deploy-website.sh
+++ dot_scripts/executable_deploy-website.sh
@@ -1,14 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
 HUGO_PATH="$HOME/Git/obenedito.org"
 TEMP_DIR="$HOME/.tmp-webdav"
 MOUNT_PATH="/media/obenedito"
 WEBDAV_FOLDER="html-obenedito.org"
 
-rm -rf $HUGO_PATH/public
-hugo -s $HUGO_PATH --minify
-mount $MOUNT_PATH
-mkdir $TEMP_DIR
-rsync -ruvc --progress --delete --temp-dir=$TEMP_DIR $HUGO_PATH/public/ $MOUNT_PATH/$WEBDAV_FOLDER
-rmdir $TEMP_DIR
-umount $MOUNT_PATH
+rm -rf "$HUGO_PATH/public"
+hugo -s "$HUGO_PATH" --minify
+mount "$MOUNT_PATH"
+mkdir "$TEMP_DIR"
+rsync -ruvc --progress --delete --temp-dir="$TEMP_DIR $HUGO_PATH/public/" \
+  "$MOUNT_PATH/$WEBDAV_FOLDER"
+rmdir "$TEMP_DIR"
+umount "$MOUNT_PATH"
--- dot_scripts/executable_tmux-website.sh
+++ dot_scripts/executable_tmux-website.sh
@@ -1,20 +1,20 @@
-#!/bin/bash
+#!/bin/sh
 
 SESSION=$USER
 
 # New tmux session
-tmux -2 new-session -d -s $SESSION
+tmux -2 new-session -d -s "$SESSION"
 
 # New window called Hugo running 'hugo server' in website folder
-tmux new-window -t $SESSION:1 -n 'hugo'
+tmux new-window -t "$SESSION:1" -n 'hugo'
 tmux send-keys "cd ~/Git/obenedito.org" C-m
 tmux send-keys "hugo server" C-m
 
 # Back to window #0, open file with atom, run local server and open it on firefox
-tmux select-window -t $SESSION:0
+tmux select-window -t "$SESSION:0"
 tmux send-keys "cd ~/Git/obenedito.org" C-m
 tmux send-keys "atom . &" C-m
 tmux send-keys "firefox http://localhost:1313/ &" C-m
 
 # Attach session
-tmux -2 attach-session -t $SESSION
+tmux -2 attach-session -t "$SESSION"
